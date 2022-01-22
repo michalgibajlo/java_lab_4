c3803f28c7f36ecbf06c3aeebe18dbf73ea06fb3
krzys <krzysztofmuller1@gmail.com>
2022-01-22 17:04:34 +0100

    ćw 1

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
         /* zad.1
         a) Stwożyć interfejs Budynek,
         b) interfejs powinien mieć metody:
-            - double powierzchnia(double pow);
-            - String adresBudynku(String adres);
-            - int liczbaOkien(int value);
-            - void liczbaMieszkancow(int value);
-            - void kolorDomu(KolorEnum kolor);
+            - double powierzchnia(double powBudynku, double powPodworka, double powParkingu);
+            - String adresBudynku(String miejscowosc, String ulica, int nrDomu, String nrLokalu String kodPocztowy);
+            - void liczbaOkien(int value); // użycie jak setter
+            - void liczbaMieszkancow(int value); // użycie jak setter
+            - void kolorDomu(KolorEnum kolor); // użycie jak setter
+            - double wyliczCene(double powierzchnia, double cena);
         c) stworzyć enum KolorEnum z przykładowymi kolorami,
         d) stworzyć jedną metodę z 2 parametrami wymyśloną przez siebie,
         */
@@ -18,14 +19,16 @@ public class Main {
         b) zaimplementować wszystkie metody,
         c) stworzyć brakujące pola(prywatne), gettery i settery, napisać metodę toString
         (gettery i settery najlepiej wygenerować: Code/Prawy myszy --> Generate --> getters/setters/toString)
-        d) stworzyć jedną dowolną metodę przez siebie
+        d) stworzyć jedną dowolną metodę przez siebie,
         */
 
         /* zad.3
         a) w klasie Main stworzyć 2 objekty typu Dom deklarując pierwszy objekt Typem Dom,
         drugi objekt typem Budynek,
-        b) wykorzystać wszystkie metody do implementacji pól
-        c) wyświetlić w konsoli metodę toString (na obu objektach) opisującą zachowanie klasy Dom
+        b) wykorzystać wszystkie metody do implementacji pól,
+        c) wyświetlić w konsoli metodę toString (na obu objektach) opisującą zachowanie klasy Dom,
+        d) sprawdzić czy idzie wywołać z obu instancji objektu (deklaracja z interfejsu i z klasy)
+        metodę której nie ma zadeklarowane w interfejsie Budynek, ale jest dopisana w klassie Dom,
         */
     }
 }
