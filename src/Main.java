0170d8040a05cf676449767f7a601e579c64bdab
krzys <krzysztofmuller1@gmail.com>
2022-01-22 17:11:53 +0100

    ćw 2

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,31 @@ public class Main {
     public static void main(String[] args) {
 
         /* zad.1
-        a) napisać interfejs CzlowiekI
-        b) zadeklarować 2 metody
+        a) napisać interfejs CzlowiekI,
+        b) zadeklarować 2 metody,
 
-        c) napisać interfejs StudentI który dziedziczy po interfejsie CzlowiekI
-        d) zadeklarować 3 metody
+        c) napisać interfejs StudentI który dziedziczy po interfejsie CzlowiekI,
+        d) zadeklarować 3 metody,
 
-        e) napisać interfejs PracownikI który dziedziczy po interfejsie CzlowiekI
-        f) zadeklarować 3 metody
+        e) napisać interfejs PracownikI który dziedziczy po interfejsie CzlowiekI,
+        f) zadeklarować 3 metody,
          */
 
         /* zad.2
-        a) napisać klasę Student która będzie implementować interfejs StudentI
-        b) napisać klasę Pracownik która będzie implementować interfejs PracownikI
+        a) napisać klasę Student która będzie implementować interfejs StudentI,
+        b) napisać klasę Pracownik która będzie implementować interfejs PracownikI,
         c) stworzyć pola, gettery i settery, metodę toString dla obu klas, (constructor z przeciążeniem - jak ktoś chce ),
-        (gettery i settery / toString() ..., najlepiej wygenerować: Code/Prawy myszy-->Generate-->getters/setters/toString)
+        (gettery i settery / toString() ..., najlepiej wygenerować:
+        --Code/Prawy myszy-->Generate-->getters/setters/toString),
         */
 
         /* zad.3
-        a) stworzyć 2 objekty typu Student i uzupełnić wszystkie pola
-        b) stworzyć 2 objekty typu Pracownik i uzupełnić wszystkie pola
-        c) wypisać metody toString() wszystkich objektów
+        a) stworzyć 2 objekty typu Student i uzupełnić wszystkie pola,
+        b) stworzyć 2 objekty typu Pracownik i uzupełnić wszystkie pola,
+        c) wypisać metody toString() wszystkich objektów,
+        */
 
-        zad.4 - Dla chętnych
+        /* zad.4* - Dla chętnych (bez dodatkowych punktów),
         a) stworzyć klasę Kierownik która będzie implementować interfejs PracownikI,
         b) zaimplementować wszystkie metody, i stworzyć pola
         c) stworzyć 2 objekty typu Kierownik i uzupełnić wszystkie pola,
@@ -34,9 +36,11 @@ public class Main {
         f) wypisać pętlą metody toString() wszystkich elementów listy
         */
 
-        /* zad5 - Dla chętnych
-        Stworzyć klasę która będzie implementować 2 lub 3 interfejsy następnie będzie
-        dziedziczona przez inną klasę
+        /* zad5* - Dla chętnych (bez dodatkowych punktów),
+        a) stworzyć klasę która będzie implementować 2 lub 3 interfejsy następnie będzie
+        dziedziczona przez inną klasę,
+        b) dokończyć implementację metod z interfejsów,
+        c) stworzyć pola w metodzie main() i wywołać metody
         */
     }
 }
